f87a88def2fc83260fe7b6b4d620fa04cbbed38b
genius <e.a.b.damman@student.tudelft.nl>
2015-10-13 10:35:16 +0200

    fixed init for knx(i think)
    fixed init and added comments for knxsensors

--- src/knxsensors.cpp
+++ src/knxsensors.cpp
@@ -14,18 +14,35 @@ KnxSensors::~KnxSensors()
     //dtor
 }
 
+/**
+ * init initialises kills all eibd processes, then starts an eibd process as user pi
+ * @brief KnxSensors::init is used to start eibd as user pi for knx
+ */
 void KnxSensors::init(){
 	FILE *in;
 	if(!(in = popen("killall eibd", "r"))){
 		return;
 	}
+        if(!(in = popen("user pi", "r"))){
+                return;
+        }
+        if(!(in = popen("eibd -i ipt:10.0.2.2", "r"))){
+                return;
+        }
 	//login als pi en dan: 'eibd -i ipt:10.0.2.2' uitvoeren
 	pclose(in);
 }
 
-void KnxSensors::setValues(int a, int b, int c){
+/**
+ * setvalues creates a string that are then thrown into the console
+ * @brief KnxSensors::setValues sets the rgb values for the knx led.
+ * @param r integer value for the red part, value can be between 0 and 255.
+ * @param g integer value for the green part, value can be between 0 and 255.
+ * @param b integer value for the blue part, value can be between 0 and 255.
+ */
+void KnxSensors::setValues(int r, int g, int b){
 	FILE *in;
-	std::string command ="groupwrite ip:localhost 1/2/4 " + func::toString(a) + " " + func::toString(b) + " " + func::toString(c);
+        std::string command ="groupwrite ip:localhost 1/2/4 " + func::toString(r) + " " + func::toString(g) + " " + func::toString(b);
 	
 	if(!(in = popen(command.c_str(), "r"))){
 		return;
