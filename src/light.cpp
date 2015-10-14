561855879fd10d6f8aa971160ffe4e5ba4ac38c3
Saliem <saliemamirkhan@hotmail.com>
2015-10-14 16:41:09 +0200

    comments added (doxygen style)

--- src/light.cpp
+++ src/light.cpp
@@ -2,14 +2,6 @@
 #include "arduPi.h"
 #include <iostream>
 
-void Light::writeLamp(int pinA, bool status)
-{
-    if (status)
-        digitalWrite(pinA, HIGH);
-    else
-        digitalWrite(pinA, LOW);
-}
-
 Light::Light(int a) : pin(a)
 {
 	status = false;
@@ -22,11 +14,33 @@ Light::~Light()
     //dtor
 }
 
+/**
+ * @brief Light::writeLamp uses arduPi library to set the given pin to the given status.
+ * @param pinA selects the physical pin.
+ * @param status selects the boolean status (either true or false) to set the pin to.
+ */
+
+void Light::writeLamp(int pinA, bool status)
+{
+    if (status)
+        digitalWrite(pinA, HIGH);
+    else
+        digitalWrite(pinA, LOW);
+}
+
+/**
+ * @brief Light::on sets status value to true and uses writeLamp() to set the physical pin on true.
+ */
+
 void Light::on()
 {
     status = true;
     writeLamp(pin, status);
 }
+
+/**
+ * @brief Light::off sets status value to false and uses writeLamp() to set the physical pin to false.
+ */
 
 void Light::off()
 {
@@ -34,11 +48,20 @@ void Light::off()
     writeLamp(pin, status);
 }
 
+/**
+ * @brief Light::toggle toggles the status value between true or false and uses writeLamp() to set the physical pin to match the status value.
+ */
+
 void Light::toggle()
 {
     status = (status ? false : true);
     writeLamp(pin, status);
 }
+
+/**
+ * @brief Light::getStatus returns the saved status of lamp.
+ * @return returns boolean variable, status, which can be true or false.
+ */
 
 bool Light::getStatus() const
 {
